79be51645f070105f2e892913102c54339e06d49
FRZ <muhammadfarizh31@gmail.com>
2024-04-18 20:21:38 +0700

    import asset (mc)

--- Assets/Code/Scripts/Soul/SoulTypeController.cs
+++ Assets/Code/Scripts/Soul/SoulTypeController.cs
@@ -11,13 +11,13 @@ public enum SoulType
 
 public class SoulTypeController : MonoBehaviour
 {
-    [SerializeField] private Material _pureMaterial;
-    [SerializeField] private Material _lostMaterial;
+    // [SerializeField] private Material _pureMaterial;
+    // [SerializeField] private Material _lostMaterial;
 
     [SerializeField] private SoulType _soulType;
     [SerializeField] private float _timeToLost;
     private float _timeRemaining;
-    private MeshRenderer _mesh => GetComponent<MeshRenderer>();
+    // private MeshRenderer _mesh => GetComponent<MeshRenderer>();
     
     public SoulType SoulType {
         get => _soulType;
@@ -25,12 +25,12 @@ public class SoulTypeController : MonoBehaviour
         {
             if(value == SoulType.PURE)
             {
-                _mesh.material = _pureMaterial;
+                // _mesh.material = _pureMaterial;
                 _timeRemaining = _timeToLost;
             }
             else
             {
-                _mesh.material = _lostMaterial;
+                // _mesh.material = _lostMaterial;
             }
             _soulType = value;
         }
