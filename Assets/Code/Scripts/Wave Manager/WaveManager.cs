8baabe634c060565c6a994c212fbe1c00ffd9c92
SandhyaTriwahyuni <142826593+SandhyaTriwahyuni@users.noreply.github.com>
2024-06-06 14:14:49 +0700

    add vfx timesup

--- Assets/Code/Scripts/Wave Manager/WaveManager.cs
+++ Assets/Code/Scripts/Wave Manager/WaveManager.cs
@@ -23,14 +23,18 @@ public class WaveManager : MonoBehaviour
 
     public int CurWaveMaxSouls { get; private set; }
 
+    [SerializeField] private GameObject vfx10Seconds;
+
     private UIManager _ui;
     private UpgradeManager _upgrade;
-    
+
     public static WaveManager Instance;
-    
+
     public float ExtraWaveTime;
 
     public int CurPowerUps;
+
+    private bool _vfxShown;//VFX
 
     private void Awake()
     {
@@ -43,9 +47,9 @@ public class WaveManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        
+
         SetCurrentData();
-        
+
         GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
     }
 
@@ -63,7 +67,7 @@ public class WaveManager : MonoBehaviour
     private void GameManagerOnGameStateChanged(GameState state)
     {
         StopAllCoroutines();
-        
+
         if (state == GameState.Gameplay)
         {
             StartCoroutine(WaveTimer());
@@ -72,9 +76,18 @@ public class WaveManager : MonoBehaviour
 
     private IEnumerator WaveTimer()
     {
+        bool _vfxShown = false;
+
         while (CurWaveTime > 0)
         {
             CurWaveTime -= Time.deltaTime;
+
+            if (CurWaveTime <= 11.0f && !_vfxShown) //vfx
+            {
+                AudioManager.Instance.PlayTimesUpSFX();
+                StartCoroutine(FlashVFX());
+                _vfxShown = true;
+            }
 
             if (GameManager.Instance.SoulCollected >= CurWaveTarget)
             {
@@ -89,15 +102,30 @@ public class WaveManager : MonoBehaviour
                     CurWaveId += 1;
                 }
             }
-
             yield return null;
         }
-        
+
         if (GameManager.Instance.SoulCollected <= CurWaveTarget)
         {
             GameManager.Instance.UpdateGameState(GameState.Lose);
             _ui.LoseCanvas();
         }
+    }
+
+    private IEnumerator FlashVFX() //vfx
+    {
+        float flashDuration = 0.5f;
+        int flashCount = 10;
+
+        for (int i = 0; i < flashCount; i++)
+        {
+            vfx10Seconds.SetActive(true);
+            yield return new WaitForSeconds(flashDuration);
+            vfx10Seconds.SetActive(false);
+            yield return new WaitForSeconds(flashDuration);
+        }
+
+        vfx10Seconds.SetActive(true);
     }
 
     public void AddExtraTime(float extraTime)
